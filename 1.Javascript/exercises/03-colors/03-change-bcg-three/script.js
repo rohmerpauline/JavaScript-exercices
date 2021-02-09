666aa8a53883f9006e00071053fe771f51da4278
Pauline Rohmer <77145751+rohmerpauline@users.noreply.github.com>
2021-02-09 23:23:03 +0100

    add comments

--- 1.Javascript/exercises/03-colors/03-change-bcg-three/script.js
+++ 1.Javascript/exercises/03-colors/03-change-bcg-three/script.js
@@ -14,7 +14,6 @@
     const setBg = () => {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = "#" + randomColor;
-        color.innerHTML = "#" + randomColor;
       }
       document.getElementById("run").addEventListener("click", setBg);
       setBg();
